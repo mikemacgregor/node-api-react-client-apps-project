e4874138fa44957298561fa04a7ddc8f62f88ef4
mikemacgregor <mike.macgregor@rogers.com>
2020-12-16 22:50:03 -0500

    modal mess

--- client/src/components/Players/styles.jsx
+++ client/src/components/Players/styles.jsx
@@ -1,13 +1,14 @@
 /* not sure if this is best way to do this or if I even like this structure, 
    but in trying to combine Bootstrap with styled components led me to this: 
    https://codesandbox.io/s/2vpm40qk90?file=/index.js:63-102
-   and decided to keep it since it "works", for now */
+   and decided to keep it, for now, since it "works" */
 
 import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const StyledButton = 
   styled(Button)`
+    margin-top: 10px;
     float: right;
   `;
 
